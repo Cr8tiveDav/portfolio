4b6f899fbd80150df6cea906680cfb5d3704ffb4
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-02-05 22:07:51 +0100

    feat: tweak About Me section UI

--- components/home/AboutMe.tsx
+++ components/home/AboutMe.tsx
@@ -1,14 +1,33 @@
-import { techStack } from '@/utils/links';
-import React from 'react';
-import { Button } from '../ui/button';
+'use client';
+import { techStack } from '@/utils/data';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import { div } from 'framer-motion/client';
 
 const AboutMe = () => {
+  const [selectedTechCategory, setSelectedTechCategory] =
+    useState<string>('all');
+  const [activeButton, setActiveButton] = useState<number>(0);
+
+  // Get all tech categories from tech stack arr
+  const techCategory = [
+    'all',
+    ...new Set(techStack.map((tech) => tech.category)),
+  ];
+
+  const filteredTechStack = techStack.filter(
+    (tech) => selectedTechCategory === tech.category
+  );
+
+  // Set as filtered tech stack or fallback to tech stack if filtered === []
+  const displayedTechStack =
+    filteredTechStack.length > 0 ? filteredTechStack : techStack;
+
   return (
-    <section className='h-[9999]'>
-      <h2 className='text-2xl text-center font-semibold mb-6'>About Me</h2>
-      <article className='bg-black/10 dark:bg-black/40 border border-blue-700 rounded-md p-6 mb-10 text-[1.1rem]'>
+    <section className='py-16'>
+      <h2 className='text-3xl md:text-5xl dark:text-blue-100 text-center font-semibold mb-8'>
+        About Me
+      </h2>
+      <article className='max-w-5xl mx-auto bg-black/10 dark:bg-black/40 border border-blue-700 rounded-md p-6 mb-16 text-[1.1rem]'>
         <p>
           I&apos;m a Frontend Developer dedicated to crafting aesthetic,
           pixel-perfect interfaces that don&apos;t just look great, but feel
@@ -17,20 +36,39 @@ const AboutMe = () => {
           every pixel has a purpose.
         </p>
       </article>
-      <h2 className='text-2xl text-center font-semibold mb-6'>Tech Stack</h2>
+      <h2 className='text-2xl md:text-4xl dark:text-blue-100 text-center font-semibold mb-8'>
+        Tech Stack
+      </h2>
 
-      <article className='grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4'>
-        {techStack.map((tech) => {
+      {/* Tech stack filter button */}
+      <div className='flex justify-center mb-8'>
+        {techCategory.map((category, i) => {
+          return (
+            <button
+              key={i}
+              className={`px-4 py-2 capitalize cursor-pointer border-b  ${activeButton === i ? 'text-blue-700 border-b-2 border-blue-700' : 'hover:border-blue-300 dark:hover:border-blue-500/30 hover:border-b-2'} transition duration-150`}
+              onClick={() => {
+                setSelectedTechCategory(category);
+                setActiveButton(i);
+              }}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+
+      {/* Tech stack */}
+      <article className='grid grid-cols-[repeat(auto-fit,130px)] sm:grid-cols-[repeat(auto-fit,150px)] justify-center gap-4'>
+        {displayedTechStack.map((tech) => {
           return (
             <div
               key={tech.id}
-              className='h-28 flex flex-col justify-center items-center border rounded-sm'
+              className='h-28 flex flex-col justify-center items-center bg-white/90 dark:bg-white/15 rounded-md drop-shadow-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105'
             >
               <Image
                 src={tech.url}
                 alt={tech.name}
-                // fill
-                // sizes='(max-width: 768px) 100vw, (max-width: 1100px) 50vw, 33vw'
                 width={100}
                 height={100}
                 priority
