bb4c0976fa57706f387007293b0c180903e0bfd8
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-02-07 21:20:31 +0100

    feat: Add the footer section

---
+++ components/home/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SocialLinks from '../social/SocialLinks';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <hr />
+      <section className='my-8 text-muted-foreground flex flex-col items-center gap-4'>
+        <div>
+          <p className='text-sm'>
+            © {currentYear} David Ezeike. All rights reserved.
+          </p>
+        </div>
+        <SocialLinks className='gap-2' />
+        <p className='text-xs'>
+          Built with{' '}
+          <span role='img' aria-label='love'>
+            ❤️
+          </span>{' '}
+          by Cr8tiveDav
+        </p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
