1b9d8cb7bb9ff0aa794f927e4728f2d34e882c5e
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-02-15 17:49:08 +0100

    refactor: adjust project preview card for better UX

--- components/projects/ProjectPreview.tsx
+++ components/projects/ProjectPreview.tsx
@@ -16,6 +16,7 @@ import { urlFor } from '@/app/sanity/image';
 import Image from 'next/image';
 import { PortableText, toPlainText } from 'next-sanity';
 import { Badge } from '../ui/badge';
+import ProjectDetailsDialog from './ProjectDetails';
 
 const ProjectPreview = ({ project }) => {
   const plainText = toPlainText(project.body);
@@ -24,20 +25,21 @@ const ProjectPreview = ({ project }) => {
     plainText.length > 150
       ? toPlainText(project.body).substring(0, 150) + '...'
       : plainText;
+
   return (
-    <Card className='mx-auto w-full max-w-sm bg-white/90 drop-shadow-md dark:bg-black/40 pt-0 hover:border-blue-700 hover:scale-105 hover:drop-shadow-xl transition ease-in-out overflow-hidden'>
+    <Card className='mx-auto w-full max-w-sm bg-white/90 dark:bg-black/40 pt-0 group hover:border-blue-700 hover:-translate-y-1 transition-all duration-1000 ease-in-out overflow-hidden'>
       <div>
         <Image
           src={urlFor(project.image).width(600).height(400).url()}
           alt={project.title}
           width={600}
           height={400}
-          className='w-full h-full object-cover rounded-t-md'
+          className='w-full h-full object-cover rounded-t-md group-hover:scale-105 transition-transform duration-1000'
         />
       </div>
-      <CardHeader>
+      <CardHeader className=''>
         <CardTitle className='text-xl'>{project.title}</CardTitle>
-        <CardDescription className=''>
+        <CardDescription>
           {excerpt}
 
           <div className='flex flex-wrap gap-2 mt-4'>
@@ -49,26 +51,30 @@ const ProjectPreview = ({ project }) => {
               );
             })}
           </div>
-          <hr className='mt-5' />
         </CardDescription>
       </CardHeader>
-      <CardFooter className=''>
-        <div className='w-full flex gap-2'>
-          <Button size='icon-lg' variant='outline' className='cursor-pointer'>
-            <FiExternalLink />
-          </Button>
-          <Button
-            size='lg'
-            variant='outline'
-            className=' font-semibold cursor-pointer'
-          >
-            <FiGithub />
-            <p>Repository</p>
-          </Button>
+      <CardFooter className='flex flex-col items-start gap-6'>
+        <hr className='w-full' />
+        <div className='w-full flex'>
+          <div className='w-full flex gap-2'>
+            <Button
+              size='icon-lg'
+              variant='outline'
+              className='cursor-pointer hover:text-blue-700'
+            >
+              <FiExternalLink />
+            </Button>
+            <Button
+              size='lg'
+              variant='outline'
+              className='font-semibold cursor-pointer hover:text-blue-700'
+            >
+              <FiGithub />
+              <p>Repository</p>
+            </Button>
+          </div>
+          <ProjectDetailsDialog project={project} />
         </div>
-        <Button size='icon-lg' variant='outline' className='cursor-pointer'>
-          <IoIosInformationCircleOutline />
-        </Button>
       </CardFooter>
     </Card>
   );
