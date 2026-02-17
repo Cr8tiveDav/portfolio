c4a70ebba968c8e26947647ebeddb6ba0c2bc30b
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-02-17 10:16:40 +0100

    feat: export GROQ type result

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,9 @@
 export type ActionResult = { message: string } | undefined;
+import { PROJECTS_QUERY_RESULT } from '@/app/sanity/types';
 
 export type ActionFunction = (
   prevState: any,
   formdata: FormData
 ) => Promise<ActionResult>;
+
+export type ProjectProps = PROJECTS_QUERY_RESULT[number];
