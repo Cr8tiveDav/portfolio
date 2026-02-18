b78051e6a1a49d3db035964a04ba9b1c518c4774
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-02-18 17:39:27 +0100

    feat: show top 3 tech tags and n more count

--- components/projects/ProjectPreview.tsx
+++ components/projects/ProjectPreview.tsx
@@ -20,65 +20,69 @@ import { ProjectProps } from '@/utils/types';
 const ProjectPreview = ({ project }: { project: ProjectProps }) => {
   const plainText = Array.isArray(project.body) && toPlainText(project.body);
 
-  const excerpt =
-    plainText && plainText.length > 150
-      ? Array.isArray(project.body) &&
-        toPlainText(project.body).substring(0, 150) + '...'
-      : plainText;
+  // const excerpt =
+  //   plainText && plainText.length > 100
+  //     ? Array.isArray(project.body) &&
+  //       toPlainText(project.body).substring(0, 100) + '...'
+  //     : plainText;
   const projectImageUrl = project.image
     ? urlFor(project.image).width(600).height(400).url()
     : null;
 
   return (
-    <Card className='mx-auto w-full max-w-sm bg-white/90 dark:bg-black/40 pt-0 group hover:border-blue-700 hover:-translate-y-1 transition-all duration-1000 ease-in-out overflow-hidden'>
+    <Card className='w-full sm:max-w-xl lg:max-w-sm relative bg-white dark:bg-black pt-0 group hover:border-blue-700 hover:shadow-xl hover:-translate-y-1 transition-all duration-1000 ease-in-out overflow-hidden'>
       <div>
         {projectImageUrl && (
           <Image
             src={projectImageUrl}
             alt={project.title || 'Project Image'}
             width={600}
-            height={400}
-            className='w-full h-full object-cover rounded-t-md group-hover:scale-105 transition-transform duration-1000'
+            height={230}
+            className='w-full h-full aspect-video object-cover rounded-t-md group-hover:scale-105 transition-transform duration-1000 cursor-pointer'
           />
         )}
       </div>
       <CardHeader className=''>
         <CardTitle className='text-xl'>{project.title}</CardTitle>
         <CardDescription>
-          {excerpt}
-
+          <p className='line-clamp-2'>{plainText}</p>
           <div className='flex flex-wrap gap-2 mt-4'>
-            {project.techStack?.map((tech, i) => {
+            {project.techStack?.slice(0, 3).map((tech, i) => {
               return (
                 <Badge variant='secondary' key={i} className='font-medium'>
                   {tech}
                 </Badge>
               );
             })}
+            {project.techStack && project.techStack.length > 3 && (
+              <Badge variant='outline' className='font-medium'>
+                {`+${project.techStack && project.techStack.length - 3} more`}
+              </Badge>
+            )}
           </div>
         </CardDescription>
       </CardHeader>
       <CardFooter className='flex flex-col items-start gap-6'>
         <hr className='w-full' />
-        <div className='w-full flex'>
-          <div className='w-full flex gap-2'>
-            <Button
-              size='icon-lg'
-              variant='outline'
-              className='cursor-pointer hover:text-blue-700'
-            >
-              <FiExternalLink />
-            </Button>
-            <Button
-              size='lg'
-              variant='outline'
-              className='font-semibold cursor-pointer hover:text-blue-700'
-            >
-              <FiGithub />
-              <p>Repository</p>
-            </Button>
+        <div className='w-full flex items-center gap-4'>
+          <Button
+            size='icon-lg'
+            variant='outline'
+            className='cursor-pointer hover:text-blue-500 shrink-0'
+          >
+            <FiExternalLink />
+          </Button>
+          <Button
+            size='lg'
+            variant='ghost'
+            className='font-medium bg-black dark:bg-white text-white dark:text-black hover:bg-primary dark:hover:bg-primary hover:text-white dark:hover:text-white cursor-pointer flex-1 flex items-center justify-center min-w-0 flex-wrap'
+          >
+            <FiGithub />
+            <p>Repository</p>
+          </Button>
+          <div className='shrink-0'>
+            <ProjectDetailsDialog project={project} />
           </div>
-          <ProjectDetailsDialog project={project} />
         </div>
       </CardFooter>
     </Card>
